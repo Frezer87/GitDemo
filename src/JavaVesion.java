c1be7e1d852e7d93f103440c6fd07415fb5613a8
Frezer <frezer.kidane@gmail.com>
2024-12-03 16:50:43 -0500

    this is commit from me

--- src/JavaVesion.java
+++ src/JavaVesion.java
@@ -3,6 +3,7 @@ public class JavaVesion {
 	
 	 public static void main(String[] args) {
 	        System.out.println("Java version: " + System.getProperty("java.version"));
+	        System.out.println("New adding a code");
 	    }
 
 }
