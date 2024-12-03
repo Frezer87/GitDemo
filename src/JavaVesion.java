8d0a54248809a1d5090236c8016799920bd444df
Frezer <frezer.kidane@gmail.com>
2024-12-03 17:03:41 -0500

    I added a new line

--- src/JavaVesion.java
+++ src/JavaVesion.java
@@ -4,6 +4,10 @@ public class JavaVesion {
 	 public static void main(String[] args) {
 	        System.out.println("Java version: " + System.getProperty("java.version"));
 	        System.out.println("New adding a code");
+	        System.out.println("Change one ");
+	        System.out.println("Change two");
+	        System.out.println("Change three");
+	        System.out.println("New changing here as well");
 	    }
 
 }
