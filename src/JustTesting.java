0b7170d1d04eee1f44c8c3d2c60c31e495d03878
Frezer <frezer.kidane@gmail.com>
2024-12-03 17:20:08 -0500

    I added a new class

---
+++ src/JustTesting.java
@@ -0,0 +1,10 @@
+
+public class JustTesting {
+
+	public static void main(String[] args) {
+		System.out.println("New changing here as well");
+		System.out.println("New changing here as well for second time");
+
+	}
+
+}
